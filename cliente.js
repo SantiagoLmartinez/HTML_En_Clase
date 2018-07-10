5e0e24b59622d1dbe349ad26b70c94cce044ff87
Glitch (ventadegolosinas) <none>
2018-07-10 20:11:10 +0000

    🏓🐃 Updated with Glitch

--- cliente.js
+++ cliente.js
@@ -1,14 +1,16 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
 class Distribuidora{
   
   static ofertar(){
 console.log('[..]Buenas Tardes, Programando en Javascrip en Buenos Aires');
 
+//document.querySelector("#panelMsg").innerHTML += "Algo! " ;
 
-    alert(12345678)
+    document.querySelector("#panelMsg").innerHTML +=
+    document.querySelector("#golosina_titulo").value 
+    + document.querySelector("#golosina_descripcion").value ;
+    
+    
+    //alert(12345678)
 
 console.log('[OK]Buenas Tardes, Programando en Javascrip en Buenos Aires');
     
