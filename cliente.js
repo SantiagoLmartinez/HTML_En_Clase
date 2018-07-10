fb9178b39785e05830a2f78d99fedc5609f06278
Glitch (ventadegolosinas) <none>
2018-07-10 19:05:14 +0000

    mas javascrip

--- cliente.js
+++ cliente.js
@@ -2,6 +2,17 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
+class Distribuidora{
+  
+  static ofertar(){
 console.log('[..]Buenas Tardes, Programando en Javascrip en Buenos Aires');
 
+
+    alert(12345678)
+
 console.log('[OK]Buenas Tardes, Programando en Javascrip en Buenos Aires');
+    
+  }
+
+}
+
